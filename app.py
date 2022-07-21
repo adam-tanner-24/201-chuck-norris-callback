6632b5a5ca0a71abbf0546a587b68bb4431ea322
Adam <adam_tanner@intuit.com>
2022-07-21 18:15:09 +0000

    test

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import dash
 from dash import dcc
 from dash import html
+from dash import Input, Output, State
 import os
 
 ###### Set up variables
@@ -20,17 +21,15 @@ app.title='Ice-Cream'
 ####### Layout of the app ########
 app.layout = html.Div([
     html.H2(heading1),
-    html.Img(src=app.get_asset_url(list_of_pics[0]), style={'width': 'auto', 'height': '10%'}),
     dcc.Dropdown(id='your-input-here',
                 options=[
-                {'label':list_of_choices[0], 'value':list_of_pics[0]},
-                {'label':list_of_choices[1], 'value':list_of_pics[1]},
-                {'label':list_of_choices[2], 'value':list_of_pics[2]},
-                {'label':list_of_choices[3], 'value':list_of_pics[3]},
-                {'label':list_of_choices[4], 'value':list_of_pics[4]},
-                {'label':list_of_choices[5], 'value':list_of_pics[5]},
+                    {'label':list_of_choices[1], 'value':list_of_pics[1]},
+                    {'label':list_of_choices[2], 'value':list_of_pics[2]},
+                    {'label':list_of_choices[3], 'value':list_of_pics[3]},
+                    {'label':list_of_choices[4], 'value':list_of_pics[4]},
+                    {'label':list_of_choices[5], 'value':list_of_pics[5]},
                 ],
-                value=list_of_pics[0],
+                value=list_of_choices[0],
                 style={'width': '500px'}),
     html.Br(),
     html.Div(id='your-output-here', children=''),
@@ -44,7 +43,9 @@ app.layout = html.Div([
 @app.callback(dash.dependencies.Output('your-output-here', 'children'),
               [dash.dependencies.Input('your-input-here', 'value')])
 def display_value(whatever_you_chose):
-    return f'You selected {whatever_you_chose} ice cream.'
+    return html.Img(src=app.get_asset_url(whatever_you_chose), style={'width': 'auto', 'height': '50%'})
+    
+
 
 
 ######### Run the app #########
